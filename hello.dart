96a316d5552e48d9b8fa7fc302e7f4b219f9e71e
Abhishek <abhishekrppandey@gmail.com>
2025-11-25 18:53:09 +0530

    Restart

--- hello.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  String text = 'Hello, Dart is fun!';
-  print(text.length); // 12
-  print(text.toUpperCase()); // DART IS FUN!
-  print(text.contains('fun')); // true
-  print(text.replaceAll('fun', 'powerful')); // Dart is powerful!
-  print(text.toLowerCase());
-}
--- hello_dart.dart
+++
@@ -1,7 +0,0 @@
-/*
-My first Dart program to practice the basics.
-*/
-void main() {
-  //This line prints a greeting to the console.
-  print('Hello, Dart!');
-}
